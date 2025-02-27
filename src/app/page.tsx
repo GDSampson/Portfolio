d3144c3df4a709a1d07221c13b926d2655ae8b1b
Daniel <sampsondg@outlook.com>
2025-02-26 22:37:05 -0400

    fixed apostrophe due to linting issue in deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center justify-center p-4 text-center">
       <div className="max-w-4xl text-white">
         <h1 className="text-4xl md:text-6xl lg:text-7xl font-light mb-4">
-          Hi, I'm <span className="text-emerald-400">Daniel</span>!
+          Hi, I{"'"}m <span className="text-emerald-400">Daniel</span>!
         </h1>
         <p className="text-2xl md:text-4xl lg:text-5xl font-light mb-12">
           A full stack web developer.
